4ab6e77b2ca4c564486409f3b01dfcf3f9c39761
Nuttavut Aukanevut <nuttavut.auk@gmail.com>
2026-01-13 02:45:14 +0700

    Update : log

--- src/app/api/document/[id]/route.ts
+++ src/app/api/document/[id]/route.ts
@@ -11,6 +11,9 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
             return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 });
         }
 
+        console.log('ไอดีไง',params,Number(params.id));
+        
+
         const doc = await prisma.document.findUnique({
             where: { id: Number(params.id) },
             include: {
